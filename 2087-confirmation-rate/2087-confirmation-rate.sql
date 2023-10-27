e76daf2d825b3367c056c3e598bfe88484e5f0d9
jongjunkim <54413901+jongjunkim@users.noreply.github.com>
2023-10-27 15:36:09 -0400

    Time: 1203 ms (77.51%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
 
-with cte as (select user_id, count(*) as total from Confirmations group by user_id),
-
-cte1 as(select user_id, count(*) as confirm from Confirmations
-where action = "confirmed" 
-group by user_id )
-
-select s.user_id, Round(ifnull(c1.confirm/c.total, 0),2) as confirmation_rate from Signups s
-left join cte c on s.user_id = c.user_id
-left join cte1 c1 on s.user_id = c1.user_id
+select s.user_id, round(avg(if(c.action = "confirmed",1,0)),2) as confirmation_rate
+from signups s left join confirmations as c on s.user_id = c.user_id group by user_id
